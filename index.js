a925b544a3a3c8e65361be06ad994d93c50a9d4f
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-01-31 12:39:16 +0800

    20230131 class

---
+++ index.js
@@ -0,0 +1,20 @@
+const port = 8000;
+const express = require("express")
+const cors = require("cors")
+const app = express();
+
+const menuRouter = require("./routes/menu.route.js");
+const catRouter = require("./routes/category.route.js");
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", menuRouter);
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "welcome Rest API" })
+})
+
+app.listen(port, () => console.log("server is running " + port))

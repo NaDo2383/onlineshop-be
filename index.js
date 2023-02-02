8da9a95fdac18bfda6914e471ede5ef58f24102f
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-02-02 20:14:49 +0800

    20230202 home

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express();
 
 const menuRouter = require("./routes/menu.route.js");
 const catRouter = require("./routes/category.route.js");
+const userRouter = require("./routes/user.route.js");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", menuRouter);
+app.use("/api", catRouter);
+app.use("/api", userRouter);
 
 app.get("/api", (req, res) => {
     res.json({ message: "welcome Rest API" });

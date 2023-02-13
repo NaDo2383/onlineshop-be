c7c1354cf2848caf783c4bf4c71b593b9ab530e5
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-02-13 12:30:26 +0800

    20230113 class

--- controllers/cate.controller.js
+++ controllers/cate.controller.js
@@ -5,93 +5,83 @@ const { parse } = require("path");
 const dataFile = process.cwd() + "/data/category.json";
 
 exports.getAll = (req, res) => {
-    fs.readFile(dataFile, "utf-8", (readErr, data) => {
-        if (readErr) {
-            return res.json({ status: false, message: readErr });
-        }
+  fs.readFile(dataFile, "utf-8", (readErr, data) => {
+    if (readErr) {
+      return res.json({ status: false, message: readErr });
+    }
 
-        const savedData = data ? JSON.parse(data) : [];
+    const savedData = data ? JSON.parse(data) : [];
 
-        return res.json({ status: true, result: savedData });
-    });
+    return res.json({ status: true, result: savedData });
+  });
 };
 
 exports.create = (req, res) => {
-    const { cateName } = req.body;
-    fs.readFile(dataFile, "utf-8", (readErr, data) => {
-        if (readErr) {
-            return res.json({ status: false, message: readErr });
-        }
+  const { cateName } = req.body;
+  fs.readFile(dataFile, "utf-8", (readErr, data) => {
+    if (readErr) {
+      return res.json({ status: false, message: readErr });
+    }
 
-        const parsedData = data ? JSON.parse(data) : [];
+    const parsedData = data ? JSON.parse(data) : [];
 
-        const newObj = { id: Date.now().toString() + "category", cateName };
+    const newObj = { id: Date.now().toString() + "category", cateName };
 
-        parsedData.push(newObj);
+    parsedData.push(newObj);
 
-        fs.writeFile(dataFile, JSON.stringify(parsedData), (writeErr) => {
-            if (writeErr) {
-                return res.json({ status: false, message: writeErr });
-            }
+    fs.writeFile(dataFile, JSON.stringify(parsedData), (writeErr) => {
+      if (writeErr) {
+        return res.json({ status: false, message: writeErr });
+      }
 
-            return res.json({ status: true, result: parsedData });
-        });
+      return res.json({ status: true, result: parsedData });
     });
+  });
 };
 
 exports.update = (req, res) => {
-    const { id, cateName } = req.body;
-    fs.readFile(dataFile, "utf-8", (readErr, data) => {
-        if (readErr) {
-            return res.json({ status: false, message: readErr });
-        }
+  const { id, cateName } = req.body;
+  fs.readFile(dataFile, "utf-8", (readErr, data) => {
+    if (readErr) {
+      return res.json({ status: false, message: readErr });
+    }
 
-        const parsedData = data ? JSON.parse(data) : [];
+    const parsedData = data ? JSON.parse(data) : [];
 
-        const updatedData = parsedData.map((e) => {
-            if (e.id == id) {
-                return { ...e, cateName };
-            } else {
-                return e;
-            }
-        });
-
-        fs.writeFile(
-            dataFile,
-            "utf-8",
-            JSON.stringify(updatedData),
-            (writeErr) => {
-                if (writeErr) {
-                    return res.json({ status: false, message: writeErr });
-                }
-
-                return res.json({ status: true, result: updatedData });
-            }
-        );
+    const updatedData = parsedData.map((e) => {
+      if (e.id == id) {
+        return { ...e, cateName };
+      } else {
+        return e;
+      }
     });
+
+    fs.writeFile(dataFile, "utf-8", JSON.stringify(updatedData), (writeErr) => {
+      if (writeErr) {
+        return res.json({ status: false, message: writeErr });
+      }
+
+      return res.json({ status: true, result: updatedData });
+    });
+  });
 };
 
 exports.delete = (req, res) => {
-    const { id } = req.params;
-    fs.readFile(dataFile, "utf-8", (readErr, data) => {
-        if (readErr) {
-            return res.json({ status: false, message: readErr });
-        }
+  const { id } = req.params;
+  fs.readFile(dataFile, "utf-8", (readErr, data) => {
+    if (readErr) {
+      return res.json({ status: false, message: readErr });
+    }
 
-        const parsedData = data ? JSON.parse(data) : [];
-        const updatedData = parsedData.filter((e) => e.id != id);
+    const parsedData = data ? JSON.parse(data) : [];
+    const updatedData = parsedData.filter((e) => e.id != id);
 
-        fs.writeFile(
-            dataFile,
-            "utf-8",
-            JSON.stringify(updatedData),
-            (writeErr) => {
-                if (writeErr) {
-                    return res.json({ status: false, message: writeErr });
-                }
+    fs.writeFile(dataFile, JSON.stringify(updatedData), (writeErr) => {
+      if (writeErr) {
+        return res.json({ status: false, message: writeErr });
+      }
 
-                return res.json({ status: true, result: updatedData });
-            }
-        );
+      return res.json({ status: true, result: updatedData });
     });
+  });
 };
